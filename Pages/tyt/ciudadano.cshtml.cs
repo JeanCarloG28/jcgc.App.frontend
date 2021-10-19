bbf827e3d020198f874fc5013f7ddf5075b379fa
jean c <jeancgoyeneche@gmail.com>
2021-10-19 18:03:46 -0500

    primera version

---
+++ Pages/tyt/ciudadano.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jcgc.App.frontend.Pages
+{
+    public class ciudadanoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
